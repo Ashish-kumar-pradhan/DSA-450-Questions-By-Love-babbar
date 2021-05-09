2724de45a3ed4ffdce9190ec947486bf9c3b1242
Ashish-kumar-pradhan <80833940+Ashish-kumar-pradhan@users.noreply.github.com>
2021-05-09 19:25:30 +0530

    s&s

---
+++ searchingAndSorting/BishuAndSoldiers.java
@@ -0,0 +1,45 @@
+package searchingAndSorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BishuAndSoldiers {
+	
+	public static void ans(int Narr[] , int Qarr[] , int n , int q) {
+		for(int i = 0 ; i < q ; i++) {
+			int count = 0;
+			int sum = 0;
+			for(int j = 0 ; j < n ; j++) {
+				if(Qarr[i] >= Narr[j]) {
+					count++;
+					sum += Narr[j];
+				}
+			}
+			System.out.println(count+ " " + sum);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int Narr[] = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+			Narr[i] = s.nextInt();
+		}
+		
+		int q = s.nextInt();
+		int Qarr[] = new int[q];
+		for(int i = 0 ; i < q ; i++) {
+			Qarr[i] = s.nextInt();
+		}
+		
+		ans(Narr , Qarr , n ,q);
+		
+		
+		
+	}
+
+}
