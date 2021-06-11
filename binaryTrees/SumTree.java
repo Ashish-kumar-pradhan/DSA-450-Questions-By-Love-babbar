68e79c9beaffdc1e7cc773d0f939d013b1c8d516
unknown <ashish.pradhane21@nsut.ac.in>
2021-06-11 16:02:06 +0530

    Sum Tree

---
+++ binaryTrees/SumTree.java
@@ -0,0 +1,40 @@
+package binaryTrees;
+
+public class SumTree {
+	
+	boolean ans = true;
+	int SumTree(Node root){
+	    
+	    if(root == null ){
+	        return 0;
+	    }
+	    
+	    int a = SumTree(root.left);
+	    int b = SumTree(root.right);
+	    int x = root.data;
+	    
+	    if(root.left != null || root.right != null){
+	        if(a + b != x){
+	            ans = false;
+	        }
+	    }
+	    
+	    return a + b + x;
+	    
+	}
+	boolean isSumTree(Node root)
+	{
+	         // Your code here
+	    if(root == null){
+	        return true;
+	    }
+	         
+	    SumTree(root);
+	    
+	    return ans;
+	    
+	}
+	
+	
+
+}
