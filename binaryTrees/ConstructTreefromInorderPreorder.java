a93d18aafd00bbf66e2bf8e40ef0b9cd239cb0a7
unknown <ashish.pradhane21@nsut.ac.in>
2021-06-11 15:13:08 +0530

    Construct Tree from Inorder & Preorder

---
+++ binaryTrees/ConstructTreefromInorderPreorder.java
@@ -0,0 +1,29 @@
+package binaryTrees;
+
+public class ConstructTreefromInorderPreorder {
+	
+	 public static Node buildTreeH(int in[], int pre[] , int inS , int inE  , int preS , int preE ){
+	        if(inS > inE){
+	            return null;
+	        }
+	        Node root = new Node(pre[preS]);
+	        int mid = -1;
+	        for(int i = 0 ; i < pre.length ; i++){
+	            if(in[i] == pre[preS]){
+	                mid = i; 
+	                break;
+	            }
+	        }
+	        root.left = buildTreeH(in, pre , inS , mid - 1  , preS + 1, mid + preS - inS);
+	        root.right = buildTreeH(in, pre, mid + 1 , inE , mid + preS - inS + 1, preE );
+	        
+	        return root;
+	    }
+	   
+	   public static Node buildTree(int in[], int pre[], int n)
+	    {
+	        // code here 
+	        return buildTreeH(in, pre, 0 , n - 1 , 0 , n - 1 );
+	    } 
+
+}
