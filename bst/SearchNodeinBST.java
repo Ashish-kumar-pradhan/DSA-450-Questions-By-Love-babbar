2cb5b70ddf9172e7628173e0ac5452f179d3d11a
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-06-13 11:38:30 +0530

    Search a node in BST

---
+++ bst/SearchNodeinBST.java
@@ -0,0 +1,22 @@
+package bst;
+
+public class SearchNodeinBST {
+	
+	boolean search(Node root, int x)
+    {
+	    // Your code here
+	    if(root == null){
+	        return false ;
+	    }
+	    
+	    if(root.data == x){
+	        return true;
+	    }
+	    else if(root.data > x){
+	        return search(root.left , x);
+	    }
+	    else{
+	        return search(root.right , x);
+	    }
+
+}
