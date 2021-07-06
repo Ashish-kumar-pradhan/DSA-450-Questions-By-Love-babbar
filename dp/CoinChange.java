6dc97f032480c29c588e0c1182d6a637689ec1f1
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-07-06 15:54:49 +0530

    coin change

--- dp/CoinChange.java
+++ dp/CoinChange.java
@@ -2,35 +2,44 @@ package dp;
 
 public class CoinChange {
 	
-	public static long count(int S[], int m, int n) { 
-	      int dp[] = new int[n+1];
-	      dp[0] = 1;
-	      for (int i = 1 ; i <= n ; i++) {
-	    	  dp[i] = 0;
-	      } 
-	      
-	      for (int j = 1 ; j < m ; j++) {
-	    	  for (int i = j ; i <= n ; i++) {
-	    		  if(S[j] == i) {
-	    			  dp[i] = dp[i] + 1;
-	    		  }
-	    		  else if (S[j] > i) {
-	    		  }
-	    		  else {
-	    			  dp[i] = dp[i] + dp[i - j];
-	    		  }
-	    		  System.out.print(dp[i] + " ");
-	    	  }
-	    	  System.out.println();
-	      }
-	      return dp[n];
+	public static long countR(int S[], int m, int n) {
+		if(n < 0) {
+			return 0;
+		}
+		if(n == 0) {
+			return 1;
+		}
+		long count = 0; 
+		for(int i = 0 ; i < m ; i++) {
+			count += countR(S , m , n - S[i]);
+		}
+		return count;
+	}
+	
+	public static long countDP(int S[], int m, int n) 
+    { 
+  
+        long dp[] = new long[n + 1];
+        dp[0] = 1;
+        
+        for(int j = 0 ; j < m ; j++ ){
+            for(int i = S[j] ; i <= n ; i++){
+                int ind = i - S[j];
+                if(ind >= 0){
+                    dp[i] += dp[ind];
+                }
+            }
+        }
+        
+        return dp[n];
+        
     } 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int S[] ={1,2,5};
-		System.out.println(count(S , 3 , 5));
+		int S[] ={1,2,3};
+		System.out.println(countR(S , 3 , 4));
 
 	}
 
