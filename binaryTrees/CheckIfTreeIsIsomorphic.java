162291bbe0e15c7ef89a22cf3880d6995a8692d3
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-08-19 19:28:58 +0530

    Check If Tree is Isomorphic

--- binaryTrees/CheckIfTreeIsIsomorphic.java
+++ binaryTrees/CheckIfTreeIsIsomorphic.java
@@ -1,6 +1,26 @@
 package binaryTrees;
 
 public class CheckIfTreeIsIsomorphic {
+	
+	boolean isIsomorphic(Node root1, Node root2)  
+    { 
+         // code here.
+         if(root1 == null && root2 == null){
+             return true;
+         }
+         if(root1 == null || root2 == null){
+             return false;
+         }
+         if(root1.data != root2.data){
+             return false;
+         }
+         
+         boolean a = isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right, root2.left);
+         boolean b = isIsomorphic(root1.left, root2.left) && isIsomorphic(root1.right, root2.right);
+         
+         return a || b; 
+         
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
