efc3215049509c4ef69e0aea79bcf3f189f6c21b
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-10-12 10:38:40 +0530

    Swap and Maximize

---
+++ greedy/SwapAndMaximize.java
@@ -0,0 +1,35 @@
+package greedy;
+
+public class SwapAndMaximize {
+	
+	long maxSum(long arr[] ,int n)
+    {
+        Arrays.sort(arr);
+        int i = 0;
+        int j = n-1;
+        long sum = 0;
+        while(i <= j){
+            if(i == j){
+                sum += (arr[j] - arr[0]);
+            }
+            else if((i+1) == j){
+                sum += (arr[j] - arr[i]);
+                sum += (arr[j] - arr[0]);
+            }
+            else {
+                sum += (arr[j] - arr[i]);
+                sum += (arr[j] - arr[i+1]);
+            }
+            i++;
+            j--;
+        }
+        
+        return sum;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
